a15103736d9fdf3648829959b6f613413fa2fe6f
amanmibra <amanmibra@gmail.com>
2017-09-28 12:45:46 -0400

    Added Redux packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import App from './containers/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
