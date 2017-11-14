b56e4fe460fea0819c85b69df3449c3ec5e344b3
amanmibra <amanmibra@gmail.com>
2017-11-14 16:43:53 -0500

    Removed class reducer and added logo images

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 
 import sidebar from "./sidebar-reducer";
-import classes from "./classes-reducer";
 import data from "./data";
 
 export default combineReducers({
   sidebar,
-  classes,
   data
 })
